ceed94bd2de814c574f0f52902c1037dff603031
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-07-15 12:20:14 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LaMiaPizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IngredientiPizza()
+        {
+            return View();
+        }
+    }
+}
